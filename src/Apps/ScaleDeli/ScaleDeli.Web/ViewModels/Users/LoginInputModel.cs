b3cc3821d8e744a69c93bcada7f343df13fa29df
Svetlana <svetli.0805@gmail.com>
2019-09-03 18:01:42 +0300

    Added View Models

---
+++ src/Apps/ScaleDeli/ScaleDeli.Web/ViewModels/Users/LoginInputModel.cs
@@ -0,0 +1,17 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ScaleDeli.Web.ViewModels.Users
+{
+    public class LoginInputModel
+    {
+        [RequiredSis]
+        public string Username { get; set; }
+
+        [RequiredSis]
+        public string Password { get; set; }
+    }
+}
---
+++ src/Apps/ScaleDeli/ScaleDeli.Web/ViewModels/Users/RegisterInputModel.cs
@@ -0,0 +1,20 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ScaleDeli.Web.ViewModels.Users
+{
+    public class RegisterInputModel
+    {
+        [RequiredSis]
+        public string Username { get; set; }
+        [RequiredSis]
+        public string Email { get; set; }
+        [RequiredSis]
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
