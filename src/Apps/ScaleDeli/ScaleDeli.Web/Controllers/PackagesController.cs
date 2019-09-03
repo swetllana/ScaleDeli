cb280e04acfb2ba3f359036add64633f4fb6797e
Svetlana <svetli.0805@gmail.com>
2019-09-04 01:25:22 +0300

    updating scaleDeli.Web

--- src/Apps/ScaleDeli/ScaleDeli.Web/Controllers/PackagesController.cs
+++ src/Apps/ScaleDeli/ScaleDeli.Web/Controllers/PackagesController.cs
@@ -1,10 +1,13 @@
-﻿using ScaleDeli.Services;
+﻿using ScaleDeli.Data.Models;
+using ScaleDeli.Services;
 using ScaleDeli.Web.ViewModels.Packages;
 using SIS.MvcFramework;
 using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Security;
 using SIS.MvcFramework.Result;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ScaleDeli.Web.Controllers
@@ -19,6 +22,7 @@ namespace ScaleDeli.Web.Controllers
             this.usersService = usersService;
         }
 
+        [Authorize]
         public IActionResult Create()
         {
             var list = this.usersService.GetUserNames();
@@ -27,12 +31,55 @@ namespace ScaleDeli.Web.Controllers
         }
 
         [HttpPost]
+        [Authorize]
         public IActionResult Create(CreateInputModel input)
         {
             if (!this.ModelState.IsValid)
             {
                 return this.Redirect("/Packages/Create");
             }
+
+            this.packagesService.Create(input.Description, input.Weight, input.ShippingAddress,input.RecipientName);
+            return  this.Redirect("/Packages/Pending");
+        }
+
+        [Authorize]
+        public IActionResult Delivered()
+        {
+            var packages = this.packagesService.GetAllByStatus(PackageStatus.Delivered)
+                .Select(x => new PackageViewModel
+                {
+                    Description = x.Description,
+                    Id = x.Id,
+                    ShippingAddress = x.ShippingAddress,
+                    Weight = x.Weight,
+                    RecipientName = x.Recipient.Username
+
+                }).ToList()  ;
+            return this.View(new PackageListViewModel { Packages = packages });
+        }
+
+        [Authorize]
+        public IActionResult Pending()
+        {
+            var packages = this.packagesService.GetAllByStatus(PackageStatus.Pending)
+                .Select(x => new PackageViewModel
+                {
+                    Description = x.Description,
+                    Id = x.Id,
+                    ShippingAddress = x.ShippingAddress,
+                    Weight = x.Weight,
+                    RecipientName = x.Recipient.Username
+
+                }).ToList();
+            return this.View(new PackageListViewModel { Packages = packages } );
+        }
+
+        [Authorize]
+        public IActionResult Deliver (string id)
+        {
+            packagesService.Deliver(id);
+            return this.Redirect("/Packages/Delivered");
         }
     }
 }
---
+++ src/Apps/ScaleDeli/ScaleDeli.Web/ViewModels/Receipts/ReceiptViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScaleDeli.Web.ViewModels.Receipts
+{
+    public class ReceiptViewModel
+    {
+        public string Id { get; set; }
+
+        public decimal Fee { get; set; }
+
+        public DateTime IssuedOn { get; set; }
+
+        public string RecipientName { get; set; }
+    }
+}
+ 
