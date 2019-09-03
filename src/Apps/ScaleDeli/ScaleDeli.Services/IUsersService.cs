518fdab7da7971ae57d8e0ec8e783a5b325de105
Svetlana <svetli.0805@gmail.com>
2019-09-03 18:05:20 +0300

    Added Services

---
+++ src/Apps/ScaleDeli/ScaleDeli.Services/IUsersService.cs
@@ -0,0 +1,16 @@
+﻿using ScaleDeli.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScaleDeli.Services
+{
+    public interface IUsersService
+    {
+        string CreateUser(string username, string email, string password);
+
+        User GetUserOrNull(string username, string password);
+    }
+
+    
+}
