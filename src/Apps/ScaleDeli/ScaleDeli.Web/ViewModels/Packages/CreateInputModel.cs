0e5be29c14743185ba38b43ad912f1ef8a155f2c
Svetlana <svetli.0805@gmail.com>
2019-09-10 13:58:11 +0300

    Added all missing elements

---
+++ src/Apps/ScaleDeli/ScaleDeli.Web/ViewModels/Packages/CreateInputModel.cs
@@ -0,0 +1,21 @@
+﻿using SIS.MvcFramework.Attributes.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ScaleDeli.Web.ViewModels.Packages
+{
+    public class CreateInputModel
+    {
+        [RequiredSis]
+        public string  Description { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        [RequiredSis]
+        public string  RecipientName { get; set; }
+    }
+}
---
+++ src/Apps/ScaleDeli/ScaleDeli.Web/ViewModels/Packages/PackageListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScaleDeli.Web.ViewModels.Packages
+{
+    public class PackageListViewModel
+    {
+        public IEnumerable<PackageViewModel> Packages { get; set; }
+    }
+}
